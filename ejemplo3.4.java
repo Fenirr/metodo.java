c3f8da22672891bbd776523615e5dbb7b9dbe21c
Fenirr <162t0305@itsm.edu.mx>
2017-02-24 11:32:58 -0600

    sin parametros con retorno
    ejemplo de método en java

---
+++ ejemplo3.4.java
@@ -0,0 +1,16 @@
+package metodo3;
+
+public class ejemplo4 {
+	public static void main(String args[]){
+	double resultado;
+	resultado= dividir();
+	System.out.println("la divicion es:"+resultado);
+}
+
+	public static double dividir(){
+	double x=4, w=7;
+	double dividir=x/w;
+	return dividir;
+}
+}
+
